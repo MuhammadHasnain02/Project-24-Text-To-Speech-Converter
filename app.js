0f8f6a4625aecce638d5c47b3060e82e2ea0ce9a
MuhammadHasnain02 <mrhasnain9009@gmail.com>
2025-11-09 11:08:47 +0500

    my-app

---
+++ app.js
@@ -0,0 +1,211 @@
+// Declare and initialize the variables where you will store all the information
+// (nine variables in total). Display in the console information about the first 
+// and last contact in the form: name/phone/email.
+
+// let contacts = [
+//     {
+//         name: "Maxwell Wright",
+//         phone: "(0191) 719 6495",
+//         email: "Curabitur.egestas.nunc@nonummyac.co.uk"
+//     },
+//     {
+//         name: "Raja Villarreal",
+//         phone: "0866 398 2895",
+//         email: "posuere.vulputate@sed.com"
+//     },
+//     {
+//         name: "Helen Richards",
+//         phone: "0800 1111",
+//         email: "libero@convallis.edu"
+//     }
+// ];
+
+// let firstName = "Maxwell Wright"
+// let secondName = "Raja Villarreal"
+// let thirdName = "Helen Richards"
+
+// let firstNum = "(0191) 719 6495"
+// let secondNum = "0866 398 2895"
+// let thirdNum = "0800 1111"
+
+// let firstEmail = "Curabitur.egestas.nunc@nonummyac.co.uk"
+// let secondEmail = "posuere.vulputate@sed.com"
+// let thirdEmail = "libero@convallis.edu"
+
+// let firstDetails = firstName + firstNum + firstEmail
+// let thirdDetails = thirdName + thirdNum + thirdEmail
+
+// console.log(firstDetails) // Display first contact
+// console.log(thirdDetails) // Display last contact
+
+
+// let age = 32;
+// age = age + 1;
+// console.log(Age);
+
+
+// let name;
+// let age;
+
+// {
+//     let height;
+// 			// 2
+//     { 					// 2
+//         { 
+//             let weight; 		// 1 	// 2
+//             console.log(name); 	// 1 	// 2
+//         }				// 2
+//         console.log(name); 		// 2
+//     }
+
+// }
+
+
+// let height = 180;
+
+// {
+//     let height = 200;
+//     height = height + 10;
+// }
+
+// console.log(height);
+
+// let x = 10 / 100;
+// console.log(typeof (x));
+
+
+// let x = 0x21
+// console.log(x);
+
+
+// let x = 100 / 0
+// console.log(x);
+
+
+// let x = 10n + 10
+// console.log(x);
+
+// let x = "Alice" + 10
+// console.log(x);
+
+// let x = "abcdefg".slice(2 , 4)
+// console.log(x);
+
+// let x = Number("1024")
+// console.log(typeof x);
+
+
+// let dog = {
+//     age: 5,
+//     name: "Axel"
+// }
+
+// console.log(dog[age]);
+// console.log(dog);
+
+// let summer = ["june" , "july" , "August"]
+// summer[1] = 7
+// console.log(summer);
+
+
+// let summer = ["June", "July", "August"];
+// let index = summer.indexOf("June");
+
+// console.log(summer);
+// console.log(index);
+
+
+// let name = ["name1" , /* "name2" */]
+// console.log(name);
+
+
+// let x = [1 , 2]
+// console.log(x);
+// x.pop()
+// console.log(x);
+
+
+// let counter = 0;
+// let userName = "John";
+
+
+// let n = 2 * 3 ** 3 - 1;
+// console.log(n);
+
+
+// let n = 2
+// console.log(n **= 3);
+// console.log(n*n*n);
+
+
+// let str = "12"
+// str = +str
+// console.log(str);
+
+// let n = 10;
+// let m = ++n;
+// console.log(n , m);
+
+
+// console.log(!( true && false || true ))
+
+
+// console.log(20 || 5)
+
+
+// console.log(false || "false");
+
+
+// let nr = "1";
+// let x = (nr === 1);
+// let y = (nr == 1);
+// let z = (nr = 1);
+// console.log(x , y , z);
+
+
+// console.log("abcd" > "Abcd");
+
+
+// console.log(3 * 4 > 20 - 15);
+//               12  >    5
+
+
+// let test = prompt("Hello", "World");
+// console.log(test);
+
+
+// let name;
+// if (test) {
+//     name = "Alice";
+// } else {
+//     name = "Bob";
+// }
+
+
+// let x = 100;
+// if (x < 100)
+//     x = 20;
+// console.log(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
