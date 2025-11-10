1cad0dbed8e04413b86859fba8215919e496acf7
MuhammadHasnain02 <mrhasnain9009@gmail.com>
2025-11-10 11:43:35 +0500

    my-app

--- app.js
+++ app.js
@@ -1,211 +1,88 @@
-// Declare and initialize the variables where you will store all the information
-// (nine variables in total). Display in the console information about the first 
-// and last contact in the form: name/phone/email.
+let speech = new SpeechSynthesisUtterance()
+let textareaInp = document.getElementById("textInput")
+let voiceSelect = document.getElementById("voiceSelect")
 
-// let contacts = [
-//     {
-//         name: "Maxwell Wright",
-//         phone: "(0191) 719 6495",
-//         email: "Curabitur.egestas.nunc@nonummyac.co.uk"
-//     },
-//     {
-//         name: "Raja Villarreal",
-//         phone: "0866 398 2895",
-//         email: "posuere.vulputate@sed.com"
-//     },
-//     {
-//         name: "Helen Richards",
-//         phone: "0800 1111",
-//         email: "libero@convallis.edu"
-//     }
-// ];
+let playBtn = document.getElementById("playBtn")
+let pauseBtn = document.getElementById("pauseBtn")
+let stopBtn = document.getElementById("stopBtn")
 
-// let firstName = "Maxwell Wright"
-// let secondName = "Raja Villarreal"
-// let thirdName = "Helen Richards"
+// ============ Array of Voices ================
+let voices = []
 
-// let firstNum = "(0191) 719 6495"
-// let secondNum = "0866 398 2895"
-// let thirdNum = "0800 1111"
+// ============ Get and Map Voices ================
 
-// let firstEmail = "Curabitur.egestas.nunc@nonummyac.co.uk"
-// let secondEmail = "posuere.vulputate@sed.com"
-// let thirdEmail = "libero@convallis.edu"
+window.speechSynthesis.onvoiceschanged = () => {
 
-// let firstDetails = firstName + firstNum + firstEmail
-// let thirdDetails = thirdName + thirdNum + thirdEmail
+    voices = window.speechSynthesis.getVoices()
+    speech.voice = voices[0]
 
-// console.log(firstDetails) // Display first contact
-// console.log(thirdDetails) // Display last contact
+    voices.forEach((voice , i) => {
+        let opt = new Option(voice.name, i)
+        voiceSelect.add(opt)
+    });
 
+}
 
-// let age = 32;
-// age = age + 1;
-// console.log(Age);
+// ============ Map and Set Selected Voice ================
 
+voiceSelect.addEventListener("change" , () => {
 
-// let name;
-// let age;
+    let index = Number(voiceSelect.value)
+    speech.voice = voices[index]
 
-// {
-//     let height;
-// 			// 2
-//     { 					// 2
-//         { 
-//             let weight; 		// 1 	// 2
-//             console.log(name); 	// 1 	// 2
-//         }				// 2
-//         console.log(name); 		// 2
-//     }
+})
 
-// }
+// ============ Play Voice ================
 
+playBtn.addEventListener("click" , () => {
 
-// let height = 180;
+    if (window.speechSynthesis.speaking) {
+        window.speechSynthesis.resume()
+        return
+    }
 
-// {
-//     let height = 200;
-//     height = height + 10;
-// }
+    speech.text = textareaInp.value
+    window.speechSynthesis.speak(speech)
 
-// console.log(height);
+})
 
-// let x = 10 / 100;
-// console.log(typeof (x));
+// ============ Pause Voice ================
 
+pauseBtn.addEventListener("click" , () => {
 
-// let x = 0x21
-// console.log(x);
+    if (window.speechSynthesis.speaking) {
+        window.speechSynthesis.pause()
+    }
 
+})
 
-// let x = 100 / 0
-// console.log(x);
+// ============ Stop Voice ================
 
+stopBtn.addEventListener("click" , () => {
+    window.speechSynthesis.cancel()
+})
 
-// let x = 10n + 10
-// console.log(x);
+// ============ Press Space and Escape Handling ================
 
-// let x = "Alice" + 10
-// console.log(x);
+document.addEventListener("keydown", (e) => {
 
-// let x = "abcdefg".slice(2 , 4)
-// console.log(x);
+    if (e.code === "Space") {
+        
+        e.preventDefault()
+        
+        if (window.speechSynthesis.speaking) {
+            window.speechSynthesis.paused ? window.speechSynthesis.resume() : window.speechSynthesis.pause()
+        }
+        else {
+            playBtn.click()
+        }
 
-// let x = Number("1024")
-// console.log(typeof x);
+    }
+    if (e.code === "Escape") stopBtn.click();
 
+});
 
-// let dog = {
-//     age: 5,
-//     name: "Axel"
-// }
 
-// console.log(dog[age]);
-// console.log(dog);
 
-// let summer = ["june" , "july" , "August"]
-// summer[1] = 7
-// console.log(summer);
-
-
-// let summer = ["June", "July", "August"];
-// let index = summer.indexOf("June");
-
-// console.log(summer);
-// console.log(index);
-
-
-// let name = ["name1" , /* "name2" */]
-// console.log(name);
-
-
-// let x = [1 , 2]
-// console.log(x);
-// x.pop()
-// console.log(x);
-
-
-// let counter = 0;
-// let userName = "John";
-
-
-// let n = 2 * 3 ** 3 - 1;
-// console.log(n);
-
-
-// let n = 2
-// console.log(n **= 3);
-// console.log(n*n*n);
-
-
-// let str = "12"
-// str = +str
-// console.log(str);
-
-// let n = 10;
-// let m = ++n;
-// console.log(n , m);
-
-
-// console.log(!( true && false || true ))
-
-
-// console.log(20 || 5)
-
-
-// console.log(false || "false");
-
-
-// let nr = "1";
-// let x = (nr === 1);
-// let y = (nr == 1);
-// let z = (nr = 1);
-// console.log(x , y , z);
-
-
-// console.log("abcd" > "Abcd");
-
-
-// console.log(3 * 4 > 20 - 15);
-//               12  >    5
-
-
-// let test = prompt("Hello", "World");
-// console.log(test);
-
-
-// let name;
-// if (test) {
-//     name = "Alice";
-// } else {
-//     name = "Bob";
-// }
-
-
-// let x = 100;
-// if (x < 100)
-//     x = 20;
-// console.log(x)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Hello brother,
+// Your current Text-to-Speech converter built with HTML, CSS, and JavaScript is working quite well. The Play and Stop buttons function properly, and the default voice is already set. Now we can make it even more functional and interactive. Here are some features and improvements you can implement:
